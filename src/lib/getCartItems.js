ae77b6312505d3631dcb12a05a41b2078b74f020
Romaan42 <Romaan4244@gmail.com>
2026-02-19 19:07:58 +0500

    update url functions

--- src/lib/getCartItems.js
+++ src/lib/getCartItems.js
@@ -1,11 +1,11 @@
 export const getCartItems = async () => {
-  const res = await fetch("http://localhost:3000/api/cart-items");
+  const res = await fetch(`${process.env.BASE_URL}/api/cart-items`);
   const data = await res.json();
   return data;
 };
 
 export const getProducts = async () => {
-  const res = await fetch("http://localhost:3000/api/product");
+  const res = await fetch(`${process.env.BASE_URL}/api/product`);
   const data = await res.json();
   return data;
 };
