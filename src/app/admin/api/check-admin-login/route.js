034ebb9144634bd1e6bb90723ca0bcd16a1addd3
Romaan42 <Romaan4244@gmail.com>
2026-02-22 16:43:39 +0500

    update the admin/products page

--- src/app/admin/api/check-admin-login/route.js
+++ src/app/admin/api/check-admin-login/route.js
@@ -2,8 +2,8 @@ import checkAdminLogin from "@/lib/checkAdminLogin";
 
 export const GET = async () => {
   const admin = await checkAdminLogin();
-  if (admin instanceof Response) {
-    return admin;
+  if (!admin) {
+    return Response.json({ login: false, message: "Admin not logged in" });
   }
   return Response.json({ login: true, message: "Welcome Roman Khan" });
 };
