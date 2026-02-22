f99926feced2cb5c407bf2f689a1d209196011e5
Romaan42 <Romaan4244@gmail.com>
2026-02-22 16:51:13 +0500

    admin product page updated

--- src/app/admin/products/page.jsx
+++ src/app/admin/products/page.jsx
@@ -4,11 +4,11 @@ import React from 'react'
 
 const page = async () => {
     const res = await fetch(`${process.env.BASE_URL}/api/product`);
-    const { data } = await res.json()
+    const result = await res.json()
 
     return (
         <div className='grid grid-cols-3 gap-10 p-10'>
-            {data.map((product) => (
+            {result?.data?.map((product) => (
                 <AdminProduct key={product._id} product={product} />
             ))}
         </div>
