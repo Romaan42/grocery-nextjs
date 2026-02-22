675535028299be3fc71981382464d961fc0ae4ce
Romaan42 <Romaan4244@gmail.com>
2026-02-22 17:28:24 +0500

    error accured

--- src/app/admin/api/check-admin-login/route.js
+++
@@ -1,9 +0,0 @@
-import checkAdminLogin from "@/lib/checkAdminLogin";
-
-export const GET = async () => {
-  const admin = await checkAdminLogin();
-  if (!admin) {
-    return Response.json({ login: false, message: "Admin not logged in" });
-  }
-  return Response.json({ login: true, message: "Welcome Roman Khan" });
-};
